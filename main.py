5cc82823391a6d3805154c98c86b06ff6d8707f7
Sergey <piteryo7@gmail.com>
2018-12-13 01:29:23 +0300

    Added catboost

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from data_processing import process_data
 from LogisticRegression import LogisticRegressionPredictor
+from CatboostPredictor import CatboostPredictor
 
-models = [LogisticRegressionPredictor({'class_weight': 'balanced'})]
+models = {'CatBoost Classifier': CatboostPredictor(
+              {
+                  "iterations": 500,
+                  "depth": 3,
+                  "learning_rate": 0.01,
+                  "l2_leaf_reg": 0.3,
+                  "rsm": 0.7,
+                  "scale_pos_weight": 4,
+                  "loss_function": 'Logloss',
+                  "eval_metric": 'AUC',
+                  "od_pval": 1e-5
+              }
+          ),
+          'Logistic Regression': LogisticRegressionPredictor({'class_weight': 'balanced'}),}
 
 def main():
     X_train, X_test, y_train, y_test, X_submission_df, X_submission = process_data()
-    for model in models:
+    for description, model in models.items():
+        print(description)
         print(model.fitModel(X_train, y_train))
         print(model.getTestScore(X_test, y_test))
         model.evaluateModel(X_submission, X_submission_df)
